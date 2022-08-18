67920133fda7bf91e871faf8d515c597e3dfbe8b
Qian Lei <chandler@stripe.com>
2022-08-18 10:58:55 -0700

    yay

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const calculateOrderAmount = (items) => {
   // Replace this constant with a calculation of the order's amount
   // Calculate the order total on the server to prevent
   // people from directly manipulating the amount on the client
-  return 1400;
-  return items.reduce((a, b) => a + b, 0);
+  return Math.max(100*(items[0] + items[1] + items[2]), 100);
 };
 
 app.post("/create-payment-intent", async (req, res) => {
